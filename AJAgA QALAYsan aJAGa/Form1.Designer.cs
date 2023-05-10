51fcc8ab9e3bbacc383e9e0ad4f5837d29ce8f04
TexnoPark <TexnoPark@107>
2023-05-10 19:48:27 +0500

    Добавьте файлы проекта.

---
+++ AJAgA QALAYsan aJAGa/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace AJAgA_QALAYsan_aJAGa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.a1 = new System.Windows.Forms.Button();
+            this.a2 = new System.Windows.Forms.Button();
+            this.a3 = new System.Windows.Forms.Button();
+            this.a4 = new System.Windows.Forms.Button();
+            this.a5 = new System.Windows.Forms.Button();
+            this.a6 = new System.Windows.Forms.Button();
+            this.a7 = new System.Windows.Forms.Button();
+            this.a8 = new System.Windows.Forms.Button();
+            this.a9 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.BackColor = System.Drawing.Color.Yellow;
+            this.flowLayoutPanel2.Controls.Add(this.a1);
+            this.flowLayoutPanel2.Controls.Add(this.a2);
+            this.flowLayoutPanel2.Controls.Add(this.a3);
+            this.flowLayoutPanel2.Controls.Add(this.a4);
+            this.flowLayoutPanel2.Controls.Add(this.a5);
+            this.flowLayoutPanel2.Controls.Add(this.a6);
+            this.flowLayoutPanel2.Controls.Add(this.a7);
+            this.flowLayoutPanel2.Controls.Add(this.a8);
+            this.flowLayoutPanel2.Controls.Add(this.a9);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(706, 225);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(343, 283);
+            this.flowLayoutPanel2.TabIndex = 1;
+            // 
+            // a1
+            // 
+            this.a1.BackColor = System.Drawing.Color.Lime;
+            this.a1.Location = new System.Drawing.Point(3, 3);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(104, 86);
+            this.a1.TabIndex = 0;
+            this.a1.UseVisualStyleBackColor = false;
+            this.a1.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // a2
+            // 
+            this.a2.BackColor = System.Drawing.Color.Lime;
+            this.a2.Location = new System.Drawing.Point(113, 3);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(104, 86);
+            this.a2.TabIndex = 1;
+            this.a2.UseVisualStyleBackColor = false;
+            this.a2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // a3
+            // 
+            this.a3.BackColor = System.Drawing.Color.Lime;
+            this.a3.Location = new System.Drawing.Point(223, 3);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(104, 86);
+            this.a3.TabIndex = 2;
+            this.a3.UseVisualStyleBackColor = false;
+            this.a3.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // a4
+            // 
+            this.a4.BackColor = System.Drawing.Color.Lime;
+            this.a4.Location = new System.Drawing.Point(3, 95);
+            this.a4.Name = "a4";
+            this.a4.Size = new System.Drawing.Size(104, 86);
+            this.a4.TabIndex = 3;
+            this.a4.UseVisualStyleBackColor = false;
+            this.a4.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // a5
+            // 
+            this.a5.BackColor = System.Drawing.Color.Lime;
+            this.a5.Location = new System.Drawing.Point(113, 95);
+            this.a5.Name = "a5";
+            this.a5.Size = new System.Drawing.Size(104, 86);
+            this.a5.TabIndex = 4;
+            this.a5.UseVisualStyleBackColor = false;
+            this.a5.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // a6
+            // 
+            this.a6.BackColor = System.Drawing.Color.Lime;
+            this.a6.Location = new System.Drawing.Point(223, 95);
+            this.a6.Name = "a6";
+            this.a6.Size = new System.Drawing.Size(104, 86);
+            this.a6.TabIndex = 5;
+            this.a6.UseVisualStyleBackColor = false;
+            this.a6.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // a7
+            // 
+            this.a7.BackColor = System.Drawing.Color.Lime;
+            this.a7.Location = new System.Drawing.Point(3, 187);
+            this.a7.Name = "a7";
+            this.a7.Size = new System.Drawing.Size(104, 86);
+            this.a7.TabIndex = 6;
+            this.a7.UseVisualStyleBackColor = false;
+            this.a7.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // a8
+            // 
+            this.a8.BackColor = System.Drawing.Color.Lime;
+            this.a8.Location = new System.Drawing.Point(113, 187);
+            this.a8.Name = "a8";
+            this.a8.Size = new System.Drawing.Size(104, 86);
+            this.a8.TabIndex = 7;
+            this.a8.UseVisualStyleBackColor = false;
+            this.a8.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // a9
+            // 
+            this.a9.BackColor = System.Drawing.Color.Lime;
+            this.a9.Location = new System.Drawing.Point(223, 187);
+            this.a9.Name = "a9";
+            this.a9.Size = new System.Drawing.Size(104, 86);
+            this.a9.TabIndex = 8;
+            this.a9.UseVisualStyleBackColor = false;
+            this.a9.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1217, 734);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Name = "Form1";
+            this.Text = "form 11";
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button a1;
+        private System.Windows.Forms.Button a2;
+        private System.Windows.Forms.Button a3;
+        private System.Windows.Forms.Button a4;
+        private System.Windows.Forms.Button a5;
+        private System.Windows.Forms.Button a6;
+        private System.Windows.Forms.Button a7;
+        private System.Windows.Forms.Button a8;
+        private System.Windows.Forms.Button a9;
+    }
+}
+
---
+++ AJAgA QALAYsan aJAGa/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AJAgA_QALAYsan_aJAGa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int sana = 0;
+        void X_0(Button btN)
+         
+        {
+            if (sana%2 == 0)
+            {
+                btN.Text = "X";
+            }
+            
+            else
+            {
+                btN.Text = "0";
+            }
+            sana++;
+            btN.Enabled = false;
+        }
+        
+       
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button ajaga= (Button)sender;
+            X_0(ajaga);
+            utti();
+         
+        }
+
+        void utti()
+        {
+            knopka(a1, a2, a3);
+            knopka(a4, a5, a6);
+            knopka(a7, a8, a9);
+            knopka(a1, a2, a3);
+            knopka(a1, a4, a7);
+            knopka(a2, a5, a8);
+            knopka(a1, a5, a9);
+            knopka(a3, a5, a7);
+        }
+        void knopka(Button b1, Button b2, Button b3)
+        {
+            if (b1.Text == "X" && b1.Text == b2.Text && b2.Text == b3.Text)
+            {
+                b1.BackColor = Color.Green;
+                b2.BackColor = Color.Green;
+                b3.BackColor = Color.Green;
+                this.BackgroundImage = Properties.Resources.photo_2023_05_10_19_17_23;
+                basilmaydi();
+
+            }
+            else if (b1.Text == "X" && b1.Text == b2.Text && b2.Text == b3.Text)
+            {
+                b1.BackColor = Color.Red;
+                b2.BackColor = Color.Red;
+                b3.BackColor = Color.Red;
+                this.BackgroundImage = Properties.Resources.nolik;
+                basilmaydi();
+
+            }
+        }
+        void basilmaydi()
+        {
+            Button[]ati=flowLayoutPanel2.Controls.OfType<Button>().ToArray();
+            foreach(Button btn in ati)
+            { btn.Enabled = false; }
+
+        }
+
+       
+    }
+}
